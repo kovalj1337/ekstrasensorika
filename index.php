79ecfae1bfc45741435d55f419003b4295e8fd0e
kovalj1337 <vanaplayers@gmail.com>
2024-08-02 21:29:40 +0400

    ll

--- index.php
+++ index.php
@@ -9,66 +9,90 @@
 
 <body>
     <?php
-    if ($_POST) {
         session_start();
-        if(isset($_SESSION['attempts'])){
-            $attempts = $_SESSION['attempts'];
+        // session_destroy();
+        if (isset($_SESSION['machine'])) {
+            $machine = $_SESSION['machine'];
+        } else {
+            $_SESSION['machine'] = $machine = rand(1, 10);
+            // echo ("Число загадано<br>");
+        }   
+        if(!isset($_SESSION["checknumb"])){
+            $_SESSION["checknumb"] = $checkNumb = [];
         }else{
+            $checkNumb = $_SESSION["checknumb"];
+        }
+        $checkNumb = [];
+        if(isset($_POST["vibor"])){
+            $numbSelect = $_POST["vibor"];
+        }else{
+            $numbSelect = 0;
+        }
+        array_push($_SESSION["checknumb"], $numbSelect);
+        if(!in_array($numbSelect, $checkNumb)){
+            $checkNumb[] = $numbSelect;
+        }
+        if (isset($_SESSION['attempts'])) {
+            $attempts = $_SESSION['attempts'];
+        } else {
             $_SESSION['attempts'] = $attempts = 3;
         }
-        if(!$attempts < 1){
-            if(isset($_SESSION['choice'])){
-                $choice = $_SESSION['choice'];
-            }else{
-                $_SESSION['choice'] = $choice = [];
-            }
-            if(isset($_SESSION['machine'])){
-                $machine = $_SESSION['machine'];
-            }else{
-                $_SESSION['machine'] = $machine = rand(1, 10);
-                echo ("Число загадано<br>");
-            }
-            if (isset($_POST["vibor"])) {
+        if (isset($_SESSION['choice'])) {
+            $choice = $_SESSION['choice'];
+        } else {
+            $_SESSION['choice'] = $choice = [];
+        }
+        if (isset($_POST["vibor"])) {
                 if ($machine == $_POST['vibor']) {
                     echo ("Ви вгадали!");
-                    echo("Число компютера " . $_SESSION['machine'] . "<br>");
-                    echo("Вам залишилося ще $attempts спроб!<br>");
-                    session_destroy();
+                    echo ("Число компютера " . $_SESSION['machine'] . "<br>");
+                    echo ("Вам залишилося ще $attempts спроб!<br>");
                 } else {
                     echo ("Ви не вгадали <br>");
                     array_push($_SESSION['choice'], $_POST['vibor']);
                 }
+                // var_dump($_SESSION['attempts']);
+                $attempts--;
+                $_SESSION["attempts"] = $attempts;
+                if(!$attempts == 0){
+                    echo("Вам залишилось $attempts спроби");
+                }else{
+                    // echo("Ваші числа:" . $_SESSION['choice'][0] )
+                    echo ("Спроби закінчились <br>");
+                    echo ("Число компютера " . $_SESSION['machine'] . "<br>");
+                }
+                // if ($attempts == 0) {
+                    //     // echo("Ваші числа:" . $_SESSION['choice'][0] )
+                //     echo ("Спроби закінчились <br>");
+                //     echo ("Число компютера " . $_SESSION['machine'] . "<br>");
+                // } else {
+                //     echo ("Залишилося $attempts спроб");
+                //     $_SESSION['attempts'] = $attempts;
+                // }
             }
-            $attempts--;
-            echo("Залишилося $attempts спроб");
-            $_SESSION['attempts'] = $attempts;
-            var_dump($_SESSION['attempts']);
-        }else{
-            // echo("Ваші числа:" . $_SESSION['choice'][0] )
-            echo("Спроби закінчились <br>");
-            echo("Число компютера " . $_SESSION['machine'] . "<br>");
-            session_destroy();
-        };
-        // for($i = 1; $i < 10; $i++){
-        //     echo '<option name="vibor" value="">';
-        // }
-    }
+            ;
+            // for($i = 1; $i < 10; $i++){
+                //     echo '<option name="vibor" value="">';
+    // }
     ?>
     <form action="index.php" method="post">
         <select name="vibor">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-        </select>
-        <button type="submit">Вибрати</button>
-        <input type="submit" name="" id="">
+        <?php 
+        for($i = 1; $i <= 10; $i++){
+            if(!in_array($i, $_SESSION["checknumb"])){
+                echo("<option value='$i'>$i</option>");
+            }else{
+                echo("<option value='$i' disabled>$i неактивно</option>");
+            }
+        };
+        echo("</select>");
+        if(!$attempts == 0){
+            echo('<button type="submit">Вибрати</button>');
+        }else{
+            echo('<button type="submit" disabled>Вибрати</button>');
+            session_destroy();
+        }
+        ?>
     </form>
 </body>
 
